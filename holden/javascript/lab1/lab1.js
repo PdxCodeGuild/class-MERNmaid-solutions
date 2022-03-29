cdd5c2a6bea49e9764e4abf3548167771dd5a176
flarespeed <hflare@gmail.com>
2022-03-29 16:09:23 -0700

    forgot to save for the part 1 push

---
+++ holden/javascript/lab1/lab1.js
@@ -0,0 +1,12 @@
+//part 1
+let name = "Holden";
+let age = 24;
+age += 1;
+
+//part 2
+function add(num1, num2) {
+  return num1 + num2;
+}
+console.log(add(12, 4));
+const hello = () => console.log("larg")
+hello()
