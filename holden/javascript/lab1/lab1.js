3cbe2eaeca9ad152870d987127cb7cc5830759bf
flarespeed <hflare@gmail.com>
2022-03-29 16:17:03 -0700

    semicolons

--- holden/javascript/lab1/lab1.js
+++ holden/javascript/lab1/lab1.js
@@ -8,8 +8,8 @@ function add(num1, num2) {
   return num1 + num2;
 }
 console.log(add(12, 4));
-const hello = () => console.log("larg")
-hello()
+const hello = () => console.log("larg");
+hello();
 const iseven = function (num){
   if (num % 2){
     return false
@@ -17,5 +17,8 @@ const iseven = function (num){
     return true
   }
 }
-console.log(iseven(12))
-console.log(iseven(15))
+console.log(iseven(12));
+console.log(iseven(15));
+
+//part 3
+const animals = ['dog', 'cat', 'llama', 'bird'];
