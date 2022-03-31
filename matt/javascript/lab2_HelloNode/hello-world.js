6010e659ace57dd061de535466d7afae5e10d9a9
matthew chimenti <matthewosu@gmail.com>
2022-03-31 16:11:41 -0700

    now title cased and use of assert

--- matt/javascript/lab2_HelloNode/hello-world.js
+++ matt/javascript/lab2_HelloNode/hello-world.js
@@ -1,7 +1,11 @@
 // Part 1: Functions
 
 //The capitalize function should take in a string and should return back the string but with the first letter capitalized.
-// const capitalize = (string) => string.toUpperCase();
+// const capitalize = (string) => {
+//     const splicedString = string.slice(1)
+//     return string.charAt(0).toUpperCase() + splicedString
+// };
+// console.log(capitalize("hello"))
 
 // The speak function should take in a name and output this message to the terminal: "Hello there $NAME!" (Replace $NAME with the result from capitialize)
 const speak = (string) => console.log(`Hello there ${string}!`);
@@ -15,7 +19,8 @@ const speak = (string) => console.log(`Hello there ${string}!`);
 const names = ["matt", "tim", "bob"]
 
 const capitalize = names.map((name) => { 
-    return " " + name.toUpperCase() ;
+    const splicedString = name.slice(1) 
+    return " " + name.charAt(0).toUpperCase() + splicedString ;
 })
 
 
--- matt/javascript/lab2_HelloNode/hello-world.test.js
+++ matt/javascript/lab2_HelloNode/hello-world.test.js
@@ -1,4 +1,10 @@
 const {capitalize, speak } = require("./hello-world")
+const assert = require('assert')
+
+// const name = capitalize("matt")
+// console.log(name)
+
+// assert.strictEqual(name, "MATT")
 
 // Part 2 injoke the functions in the new file
 // speak(capitalize("matt"))
