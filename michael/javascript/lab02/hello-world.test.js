a781f0e685533282989d394c301a55ac9ab572ce
cdmichaelb <broetjem@gmail.com>
2022-03-31 22:34:08 -0700

    added more tests

--- michael/javascript/lab02/hello-world.test.js
+++ michael/javascript/lab02/hello-world.test.js
@@ -1,24 +1,40 @@
 const assert = require("assert"); // Import assert to use it in the tests
 const helloWorld = require("./hello-world.js"); // Import helloWorld to use it in the tests
 
-const string = "llama"; // Declare a string variable
-const array = ["llama", "cat", "dog"]; // Declare an array variable
+const string = "Llama"; // Declare a string variable
+const array = ["Llama", "Cat", "Dog"]; // Declare an array variable
 
 // References: https://nodejs.org/api/assert.html
 //             https://nodejs.org/api/assert.html#assertdeepstrictequalactual-expected-message
 
 // Test capitalizeWord
-assert.strictEqual(helloWorld.capitalizeWord(string), "Llama");
+assert.strictEqual(helloWorld.capitalizeWord(string), "Llama"); // Test if the function returns the correct value
+// assert.strictEqual(helloWorld.capitalizeWord(string), "lLama"); // This test should fail
+// assert.strictEqual(helloWorld.capitalizeWord(string), "LlAMA"); // This test should fail
 
 //Test speakWord
 assert.strictEqual(
 	helloWorld.speakWord(helloWorld.capitalizeWord(string)),
 	"Hello there Llama!"
-);
+); // Test if the function returns the correct value
+// assert.strictEqual(
+// 	helloWorld.speakWord(helloWorld.capitalizeWord(string)),
+// 	"Hello Llama!"
+// ); // This test should fail
+// assert.strictEqual(
+// 	helloWorld.speakWord(helloWorld.capitalizeWord(string)),
+// 	"Llama!"
+// ); // This test should fail
 
 // Test capitalizeWords
 assert.deepStrictEqual(helloWorld.capitalizeWords(array), [
 	"Llama",
 	"Cat",
 	"Dog",
-]);
+]); // Test if the function returns the correct value
+// assert.deepStrictEqual(helloWorld.capitalizeWords(array), ["Llama", "Dog"]); // This test should fail
+// assert.deepStrictEqual(helloWorld.capitalizeWords(array), [
+// 	"Dog",
+// 	"Cat",
+// 	"Llama",
+// ]); // This test should fail
