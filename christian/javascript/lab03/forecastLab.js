62a744a90df628d53d252e213d7197d3ceeb17b5
christian <chrisalv27@yahoo.com>
2022-04-01 18:09:04 -0700

    Working forecast lab. Sorting through data

---
+++ christian/javascript/lab03/forecastLab.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
+const apiKey = process.env.SECRET_KEY;
+const url = "https://api.openweathermap.org/data/2.5/onecall?lat=45.52&lon=-122.67&exclude=daily,minutely&units=imperial&appid=";
+
+const weather = async (url, apiKey) => {
+  const response = await axios.get(url + apiKey);
+  console.log(response.data.hourly[0].dt)
+  for (hour of response.data.hourly) {
+    console.log(hour.temp)
+  
+};
+
+
+}
+
+
+console.log(weather(url, apiKey))
+
+
+
+
+
+// (async () => {
+//   const portland = await weather(url, apiKey);
+
+//   const hourly = portland.map((port) => {
+//     console.log(hourly)
+//     return port.temp;
+// });
+
+
+// })();
+
