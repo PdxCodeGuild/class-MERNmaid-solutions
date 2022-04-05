de7e3d969a852ac5c508c41619c6a95cc32abdaa
Reece Adams <r.adams8778@gmail.com>
2022-04-04 20:29:21 -0400

    fixed part 4

--- reece/javascript/lab02/hello-worldv3.js
+++ reece/javascript/lab02/hello-worldv3.js
@@ -1,13 +1,16 @@
 // Reece Adams - ADV JS - 3/29/22 - lab 3: Hello Node - Version 3 //
 
-const string = 'Martino'
+const nameArray = ['MARTINO','JUstin','mikE']
 
 const speak = (name) => console.log(`Hello there ${name}!`);
 
 const capitalize = (item) => {
-    return split = item.split()
-    // let letter = lower.charAt(0).toUpperCase();
-    // return letter + lower.substring(1);
+    const lowerName = item.toLowerCase()
+    const splitName = lowerName.split("")
+    const upperLetter = splitName[0].toUpperCase()
+    const slicedName = splitName.slice(1)
+    const capName = upperLetter + slicedName.join('')
+    return capName
 };
 
-speak(capitalize(string))
+const capArray = nameArray.map(i => speak(capitalize(i)));
