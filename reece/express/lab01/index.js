14aea2ce5db3d9c113fb67f1a5d65d66a346bea0
Reece Adams <r.adams8778@gmail.com>
2022-04-06 16:35:32 -0400

    update

--- reece/express/lab01/index.js
+++ reece/express/lab01/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const app =express();
+const app = express();
+
+const sayings = ['It is certain.', 'Most likely.', 'Reply hazy, try again.', 'Very doubtful.']
 
 app.get("/", (req, res) => {
-    res.send({response: 'Magic 8 ball here!'});
+    res.send({response: sayings});
 });
 
 app.listen(3000, () => {
