8a864a37836d4e2d5dd3d9f19ff93e46b4412d9a
cdmichaelb <broetjem@gmail.com>
2022-04-05 17:40:37 -0700

    lab 2 in progress, lab 1 fixes

--- michael/express/lab02/app.js
+++ michael/express/lab02/app.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const Fruit = require("./models/fruit");
 
-const { SECRET_DB_URL } = require("./config/.env");
-console.log(SECRET_DB_URL);
+require("dotenv").config();
+
+const SECRET_DB_URL =
+	process.env.SECRET_DB_URL + ":" + process.env.SECRET_DB_PORT + "/lab02";
+const port = process.env.SECRET_PORT || 3333;
 
 const app = express();
 
-mongoose.connect(SECRET_DB_URL);
+mongoose
+	.connect(SECRET_DB_URL)
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((err) => {
+		console.log("Error connecting to MongoDB: ", err);
+	});
+
+app.listen(port, () => {
+	console.log("Server is running on port " + port);
+});
+
+app.post("/add-fruit", async (req, res) => {
+	const fruit = new Fruit({ name: req.query.name, price: req.query.price });
+	await fruit.save();
+	res.send(fruit);
+});
+
+app.get("/fruits", async (req, res) => {
+	const fruits = await Fruit.find();
+	res.send(fruits);
+});
+
+app.get("/fruit", async (req, res) => {
+	const fruit = await Fruit.findById("624cd7376b79a7e755072746");
+	res.send(fruit);
+});
---
+++ michael/express/lab02/models/fruit.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const fruitSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+module.exports = Fruit;
