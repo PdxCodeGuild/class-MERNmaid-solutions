a709291671ea73d4ccd7ac53c40d643f4cb0ea6f
christian <chrisalv27@yahoo.com>
2022-04-07 13:38:30 -0700

    express lab02

--- christian/Express/lab02/app.js
+++ christian/Express/lab02/app.js
@@ -14,12 +14,53 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+//create person 
 app.post('/people', async (req, res) => {
-    const person = new Person(req.firstname);
-    await person.save();
-    res.send(person);
+    const person = new Person(req.body); //use body parser and pull Person model
+    await person.save(); // .save to database
+    res.send(person); //.send back 
 })
 
+//retrieve person
+app.get('/people/:id', async (req, res) => {
+    // use ':id to target specific people
+    const person = await Person.findOne({_id: req.params.id}); 
+    if (!person) {
+        res.sendStatus(404); //if person cant be retrieved, send 404
+    
+    } else {
+        res.send(person) // return data
+    }
+});
+//update person
+app.patch('people/:id', async (req, res) => {
+    //same as delete except use .patch
+    const person = await Person.findOne({_id: req.params.id});
+    if(!person) {res.send(404)};
+    const personInfo = req.body;
+    person.set(personInfo) //.set to update info
+    await person.save();
+    
+    res.send(person)
+
+});
+//delete person
+app.delete('/person/:id', async (req, res) => {
+    const person = await Person.findOne({ _id: req.params.id });
+    if (!person) {
+    res.send(404);
+    }
+    await person.remove(); // .remove deletes person
+    res.send(person);
+
+})
+//retrieve person
+app.get("/person", async (req, res) => {
+    const person = await Person.find();
+    res.send(person);
+  
+  });
+  
 
 
 
