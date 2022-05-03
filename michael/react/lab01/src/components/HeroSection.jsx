9991a8ebecf3c25abd67b7bf1f4e05932e74d454
cdmichaelb <broetjem@gmail.com>
2022-05-03 14:36:35 -0700

    File structure change

--- michael/react/lab01/src/components/HeroSection.jsx
+++ michael/react/lab01/src/components/HeroSection.jsx
@@ -123,9 +123,9 @@ export default function Example() {
 					<main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
 						<div className="sm:text-center lg:text-left">
 							<h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-								<span className="block xl:inline">Data to enrich your</span>{" "}
+								<span className="block xl:inline"> CDMichaelB's MERN </span>{" "}
 								<span className="block text-indigo-600 xl:inline">
-									online business
+									react labs
 								</span>
 							</h1>
 							<p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
@@ -139,7 +139,7 @@ export default function Example() {
 										href="#"
 										className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
 									>
-										Get started
+										First Lab
 									</a>
 								</div>
 								<div className="mt-3 sm:mt-0 sm:ml-3">
@@ -147,7 +147,7 @@ export default function Example() {
 										href="#"
 										className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
 									>
-										Live demo
+										Random Lab
 									</a>
 								</div>
 							</div>
