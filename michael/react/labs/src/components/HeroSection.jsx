09f26bf13db7884d39a455f07bff4969a0b9f878
cdmichaelb <broetjem@gmail.com>
2022-06-24 15:31:18 -0700

    Update about me

--- michael/react/labs/src/components/HeroSection.jsx
+++ michael/react/labs/src/components/HeroSection.jsx
@@ -135,9 +135,11 @@ export default function Example() {
 								</span>
 							</h1>
 							<p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-								lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-								fugiat aliqua.
+								Full stack web developer with a focus on the backend.
+								<br />
+								MERN stack web developer with a focus on the backend.
+								<br />
+								NODEJS desktop developer. Python desktop developer.
 							</p>
 							<div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
 								<div className="rounded-md shadow">
