689f8b5520ea601664a5e56c34c7620e207df493
flarespeed <hflare@gmail.com>
2022-07-07 17:17:52 -0700

    made box for who am i section

--- holden/react/lab2/src/pages/Home.jsx
+++ holden/react/lab2/src/pages/Home.jsx
@@ -7,10 +7,12 @@ const Home = () => {
   return (
     <div class="main">
       <h1>Placeholder Name</h1>
+      <div class="who">
       <h3>Who I am:</h3>
-      <div class="">
+      <div>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sodales vulputate lectus vitae bibendum. In hac habitasse platea dictumst. In pulvinar nibh malesuada erat gravida vulputate. Cras id vehicula dolor. Cras nec bibendum odio, in facilisis dolor. Pellentesque tincidunt malesuada tempus. Proin scelerisque orci arcu. Quisque condimentum urna non tincidunt auctor.</p>
         <p>Aliquam tempor tellus eget erat vulputate, sit amet congue justo consectetur. Sed nibh urna, faucibus id ex eget, fringilla scelerisque turpis. Morbi mollis turpis nec eleifend venenatis. Etiam aliquet accumsan dictum. Donec massa elit, viverra et arcu in, aliquet tempor elit. Praesent sagittis est vel nisl pellentesque tempus. Suspendisse euismod bibendum ex sed condimentum. Ut ut lacinia massa, tempor sagittis nisl. Duis arcu enim, vulputate quis congue in, euismod sit amet nisi. Nulla ac tincidunt mi. Nulla ligula metus, mollis in porta ut, elementum non nisi. Pellentesque hendrerit justo eu risus faucibus, vel dapibus mi pellentesque.</p>
+      </div>
       </div>
       <img class="pic1" src="https://avatars.githubusercontent.com/u/22627990?v=4" alt="some koala (my profile picture on github)" title="I am definitely human, this picture just is from a bad angle"/>
       <div class="horizontal">
